b897c81bcad3cee217e203606c9e65a214ef45b4
yoguo <yoguo@redhat.com>
2020-08-18 16:29:02 +0800

    update

--- libguestfs-rhel8-x86_64-compose.groovy
+++ libguestfs-rhel8-x86_64-compose.groovy
@@ -1,9 +1,57 @@
 
 def parse_ci_message() {
+    def release_arch = 'x86_64'
     sh '''
     #!/bin/bash -x
     python $WORKSPACE/xen-ci/utils/umb_pungi_parse.py
     cat $WORKSPACE/CI_MESSAGE_ENV.txt
+    
+    if [ "${release_short}" == "RHEL" ] && [ "${release_type}" == "ga" ]; then
+        echo "milestone or nightly compose"
+        distro=${compose_id}
+
+        # Check if the compose status if finished
+        if [ "${compose_type}" == "nightly" ]; then
+            compose_repo="http://download.eng.pek2.redhat.com/rhel-8/nightly/RHEL-8"
+        else
+            compose_repo="http://download.eng.pek2.redhat.com/rhel-8/rel-eng/RHEL-8"
+        fi
+        wget -O compose_status $compose_repo/${compose_id}/STATUS
+        WGET_STATUS=$?
+        compose_status=`cat compose_status`
+        until [ "$WGET_STATUS" == "0" -a "$compose_status" == "FINISHED" ]
+        do
+            echo "The new compose ${compose_id} is not ready in pek2 repo now. Waiting..."
+            sleep 10m
+            wget -O compose_status $compose_repo/${compose_id}/STATUS
+            WGET_STATUS=$?
+            compose_status=`cat compose_status`
+        done
+    else
+        # RHEL-AV (or nighlty) or updates compose
+        if [ "${compose_type}" == "nightly" ]; then
+            compose_repo="http://download-node-02.eng.bos.redhat.com/rhel-8/nightly/RHEL-8"
+        else
+            compose_repo="http://download-node-02.eng.bos.redhat.com/rhel-8/rel-eng/RHEL-8"
+        fi
+        tree_name="latest-RHEL-${release_version%.*}.0"
+        wget $compose_repo/$tree_name/COMPOSE_ID -O distro
+        distro=`cat distro`
+    fi
+
+    echo "DISTRO=$distro" > $WORKSPACE/DISTRO.txt
+
+    if [ "${release_short}" == "RHEL" ]; then
+        echo "milestone or updates or nightly compose"
+        variant="AppStream"
+    elif [ "${release_short}" == "ADVANCED-VIRT" ]; then
+        echo "rhel-av compose"
+        variant="Advanced-virt"
+    fi
+
+    wget ${location}/$variant/{release_arch}/os/Packages/ -O packages.html
+    nvr=`cat packages.html | grep -v python | egrep -oe "libguestfs-[0-9]{1}.[0-9]{2}.[0-9]{1,3}-[.0-9]{2,6}module\+el8.[0-9]{1}.[0-9]{1}\+[0-9]{,6}\+[0-9,a-z]{,10}" | awk "NR==1"`
+    echo "NVR=$nvr" >> $WORKSPACE/CI_MESSAGE_ENV.txt
     '''
 }
 
