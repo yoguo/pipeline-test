14ec522b926af85817a32350711283db8e6c530b
yoguo <yoguo@redhat.com>
2020-08-19 10:02:44 +0800

    update

--- libguestfs-rhel8-x86_64-compose.groovy
+++ libguestfs-rhel8-x86_64-compose.groovy
@@ -50,7 +50,7 @@ def parse_ci_message() {
     fi
 
     wget ${location}/$variant/x86_64/os/Packages/ -O packages.html
-    nvr=$(cat packages.html | grep -v python | egrep -oe "libguestfs-[0-9]{1}.[0-9]{2}.[0-9]{1,3}-[.0-9]{2,6}module+el8.[0-9]{1}.[0-9]{1}+[0-9]{,6}+[0-9,a-z]{,10}" | head -n 1)
+    nvr=$(cat packages.html | grep -v python | egrep -oe "libguestfs-[0-9]{1}.[0-9]{2}.[0-9]{1,3}-[.0-9]{2,6}module\\+el8.[0-9]{1}.[0-9]{1}\\+[0-9]{,6}\\+[0-9,a-z]{,10}" | head -n 1)
     echo "NVR=$nvr" >> $WORKSPACE/CI_MESSAGE_ENV.txt
     '''
 }
