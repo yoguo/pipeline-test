ec1d8c3816450da7315d069723c710b42e1b0ea1
yoguo <yoguo@redhat.com>
2020-08-19 08:37:31 +0800

    update

--- libguestfs-rhel8-x86_64-compose.groovy
+++ libguestfs-rhel8-x86_64-compose.groovy
@@ -4,7 +4,6 @@ def parse_ci_message() {
     #!/bin/bash -x
     cp -f /home/jenkins-platform/workspace/yoguo/umb_pungi_parse.py $WORKSPACE/xen-ci/utils/
     python $WORKSPACE/xen-ci/utils/umb_pungi_parse.py
-    cat $WORKSPACE/CI_MESSAGE_ENV.txt
     source $WORKSPACE/CI_MESSAGE_ENV.txt
     
     if [ "${release_short}" == "RHEL" ] && [ "${release_type}" == "ga" ]; then
@@ -51,7 +50,7 @@ def parse_ci_message() {
     fi
 
     wget ${location}/$variant/x86_64/os/Packages/ -O packages.html
-    nvr=`cat packages.html | grep -v python | egrep -oe "libguestfs-[0-9]{1}.[0-9]{2}.[0-9]{1,3}-[.0-9]{2,6}module+el8.[0-9]{1}.[0-9]{1}+[0-9]{,6}+[0-9,a-z]{,10}" | awk "NR==1"`
+    nvr=$(cat packages.html | grep -v python | egrep -oe "libguestfs-[0-9]{1}.[0-9]{2}.[0-9]{1,3}-[.0-9]{2,6}module+el8.[0-9]{1}.[0-9]{1}+[0-9]{,6}+[0-9,a-z]{,10}" | awk "NR==1")
     echo "NVR=$nvr" >> $WORKSPACE/CI_MESSAGE_ENV.txt
     '''
 }
